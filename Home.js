8cb915a20338231d4f537b6bace7398db208dc46
BALAKAVI B <118712303+balakav@users.noreply.github.com>
2023-11-25 13:21:31 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,36 @@
+// Home.js
+
+import React from 'react';
+import './Home.css'; // Import your CSS file for styling
+import {Link} from "react-router-dom"
+const Home = () => {
+  return (
+    <div className="home-page">
+      <header>
+        <h1>Welcome to Stock Management System</h1>
+      </header>
+      <main>
+        <section className="options">
+         <Link to="/In" ><div className="option">
+            <h2>Inventory Management</h2>
+            <p>Manage your inventory, add, edit or remove items.</p>
+          </div></Link>
+          <div className="option">
+            <h2>Orders</h2>
+            <p>View and manage orders, track shipments.</p>
+          </div>
+          <div className="option">
+            <h2>Reports</h2>
+            <p>Generate reports, analyze stock data.</p>
+          </div>
+          <div className="option">
+            <h2>Settings</h2>
+            <p>Configure system settings, user permissions.</p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
